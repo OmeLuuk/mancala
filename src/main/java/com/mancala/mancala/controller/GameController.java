9a5116b3f99738c0bf95d21dd3c992269f40569c
omeluuk <luuk@sterke.net>
2023-12-01 22:13:08 +0100

    Initial commit

---
+++ src/main/java/com/mancala/mancala/controller/GameController.java
@@ -0,0 +1,10 @@
+package com.mancala.mancala.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GameController {
+
+}
