509b0be5b4899b5f92b47176b3f20f7cbadaac94
omeluuk <luuk@sterke.net>
2023-12-04 12:47:24 +0100

    Add another test and refactor board logic

--- src/main/java/com/mancala/mancala/model/Board.java
+++ src/main/java/com/mancala/mancala/model/Board.java
@@ -3,16 +3,19 @@ package com.mancala.mancala.model;
 import org.springframework.web.bind.annotation.PathVariable;
 
 public class Board implements IBoard {
-    protected int[] pits = new int[14]; // 12 small pits + 2 larger pits
+    private final int boardSize = 14; // 6 pits per player and one score pit
+    private final int startStones = 6;
+    protected int[] pits;
 
     public Board() {
-        // Initialize the board with 6 stones in each small pit
-        for (int i = 0; i < 14; i++) {
-            pits[i] = 6;
+        pits = new int[boardSize];
+
+        for (int i = 0; i < boardSize; i++) {
+            pits[i] = startStones;
         }
         // Larger pits (stores) start empty
-        pits[6] = 0; // Player 1's store
-        pits[13] = 0; // Player 2's store
+        pits[boardSize / 2 - 1] = 0; // Player 1's store
+        pits[boardSize - 1] = 0; // Player 2's store
     }
 
     public String toHTML(boolean isCurrentPlayer, Player player) {
@@ -21,10 +24,11 @@ public class Board implements IBoard {
         html.append("<h1>Current Game State</h1><table><tr>");
 
         // Display player 1's larger pit
-        html.append("<td style='width: 50px; text-align: center;'><span style='color: blue;'>").append(pits[13]).append("</span></td>");
+        html.append("<td style='width: 50px; text-align: center;'><span style='color: blue;'>")
+                .append(pits[boardSize - 1]).append("</span></td>");
 
         // Display player 1's pits
-        for (int i = 12; i > 6; i--) {
+        for (int i = boardSize - 2; i >= boardSize / 2; i--) {
             html.append("<td style='width: 50px; text-align: center;'>");
             if (isCurrentPlayer && player == Player.blue) {
                 html.append("<a href='/move/").append(i).append("' style='color: blue;'>");
@@ -38,7 +42,7 @@ public class Board implements IBoard {
 
         // Display player 2's pits
         html.append("</tr><tr><td></td>"); // Skip the first cell for alignment
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < boardSize / 2 - 1; i++) {
             html.append("<td style='width: 50px; text-align: center;'>");
             if (isCurrentPlayer && player == Player.red) {
                 html.append("<a href='/move/").append(i).append("' style='color: red;'>");
@@ -51,7 +55,8 @@ public class Board implements IBoard {
         }
 
         // Display player 2's larger pit
-        html.append("<td style='width: 50px; text-align: center;'><span style='color: red;'>").append(pits[6]).append("</span></td>");
+        html.append("<td style='width: 50px; text-align: center;'><span style='color: red;'>")
+                .append(pits[boardSize / 2 - 1]).append("</span></td>");
 
         html.append("</tr></table></body></html>");
         return html.toString();
@@ -59,9 +64,11 @@ public class Board implements IBoard {
 
     // This method is designed for only two players currently.
     public boolean makeMove(int pitIndex, Player player) {
+        boolean blueTurn = player == Player.blue;
+
         boolean triedInvalidMove =
-                player == Player.blue && pitIndex < 7 ||
-                player == Player.red && pitIndex > 6 ||
+                blueTurn && pitIndex < boardSize / 2 ||
+                !blueTurn && pitIndex >= boardSize / 2 - 1 ||
                 pits[pitIndex] == 0;
         if (triedInvalidMove)
         {
@@ -72,30 +79,34 @@ public class Board implements IBoard {
         pits[pitIndex] = 0;
         int lastPitIndex = 0;
 
-        for (int i = 1; i <= stones; i++) {
+        for (int i = 1; i <= stones; i++)
+        {
             lastPitIndex = (pitIndex + i) % pits.length;
             // Skip opponent's big pit
-            if ((player == Player.blue && lastPitIndex == 6) || (player == Player.red && lastPitIndex == 13)) {
+            if ((blueTurn && lastPitIndex == boardSize / 2 - 1) ||
+                    (!blueTurn && lastPitIndex == boardSize - 1)) {
                 stones++; // Compensate for the skipped pit
                 continue;
             }
             pits[lastPitIndex]++;
         }
 
+        // Determine the index for the player's big pit
+        int bigPitIndex = blueTurn ? boardSize - 1 : boardSize / 2 - 1;
+
         // Capturing logic
-        if (lastPitIndex != 6 && lastPitIndex != 13 && pits[lastPitIndex] == 1) {
+        if (lastPitIndex != bigPitIndex && pits[lastPitIndex] == 1)
+        {
             // Check if the last pit is on the player's side and was empty before the move
-            if ((player == Player.blue && lastPitIndex > 6) || (player == Player.red && lastPitIndex < 6)) {
-                int oppositePitIndex = 12 - lastPitIndex;
+            if ((blueTurn && lastPitIndex >= boardSize / 2) || (!blueTurn && lastPitIndex < boardSize / 2 - 1))
+            {
+                int oppositePitIndex = boardSize - 2 - lastPitIndex;
                 int capturedStones = pits[oppositePitIndex] + 1;  // Capture opposite stones plus the one just placed
                 pits[oppositePitIndex] = 0;  // Empty the opposite pit
                 pits[lastPitIndex] = 0;  // Empty the last pit
-                pits[player == Player.blue ? 13 : 6] += capturedStones;  // Add to the player's store
+                pits[bigPitIndex] += capturedStones;  // Add to the player's store
             }
         }
-
-        // Determine the index for the player's big pit
-        int bigPitIndex = player == Player.blue ? 13 : 6;
 
         // Player gets another turn if the last stone lands in their big pit
         return lastPitIndex == bigPitIndex;
@@ -105,13 +116,13 @@ public class Board implements IBoard {
     {
         // Here we assume that we always play with the maximum number of players. If we want to allow games with fewer
         // players we will need to store the number of players and loop until that number instead.
-        if (7 * Player.values().length != pits.length) {
+        if (boardSize / 2 * Player.values().length != pits.length) {
             throw new IllegalArgumentException("The number of players does not match the board's capacity!");
         }
         for (int player = 0; player < Player.values().length; player++)
         {
             boolean pitsEmpty = true;
-            for (int pit = 7 * player; pit < 7 * (player + 1) - 1; pit++)
+            for (int pit = boardSize / 2 * player; pit < boardSize / 2 * (player + 1) - 1; pit++)
             {
                 if (pits[pit] != 0)
                 {
@@ -131,7 +142,7 @@ public class Board implements IBoard {
     public int getStoreCount(Player player) {
         // Calculate the index of the store pit based on the player's ordinal
         // This assumes a specific ordering and spacing in the pits array
-        int storeIndex = 6 + 7 * player.ordinal();
+        int storeIndex = boardSize / 2 - 1 + (boardSize / 2 * player.ordinal());
         if (storeIndex < pits.length)
         {
             return pits[storeIndex];
--- src/test/java/com/mancala/mancala/model/GameTest.java
+++ src/test/java/com/mancala/mancala/model/GameTest.java
@@ -53,6 +53,53 @@ class GameTest {
         assertTrue(game.isGameOver(), "Game should be over after the last move");
 
         // Assuming Blue is the winner
-        assertArrayEquals(new Player[]{Player.blue}, game.getWinners(), "Blue should be the winner after the game over");
+        assertArrayEquals(new Player[]{Player.red}, game.getWinners(), "Red should be the winner after the game over");
+    }
+
+    @Test
+    void testManyConsecutiveMoves() {
+        testBoard.setPits(new int[]{6, 6, 6, 6, 6, 6, 0, 6, 6, 6, 6, 6, 6, 0});
+
+        game.makeMove(7, Player.blue);
+        assertArrayEquals(new int[]{6, 6, 6, 6, 6, 6, 0, 0, 7, 7, 7, 7, 7, 1}, testBoard.getPits());
+
+        game.makeMove(8, Player.blue);
+        assertArrayEquals(new int[]{7, 7, 6, 6, 6, 6, 0, 0, 0, 8, 8, 8, 8, 2}, testBoard.getPits());
+
+        game.makeMove(0, Player.red);
+        assertArrayEquals(new int[]{0, 8, 7, 7, 7, 7, 1, 1, 0, 8, 8, 8, 8, 2}, testBoard.getPits());
+
+        game.makeMove(7, Player.blue);
+        assertArrayEquals(new int[]{0, 8, 7, 7, 0, 7, 1, 0, 0, 8, 8, 8, 8, 10}, testBoard.getPits());
+
+        game.makeMove(3, Player.red);
+        assertArrayEquals(new int[]{0, 8, 7, 0, 1, 8, 2, 1, 1, 9, 9, 8, 8, 10}, testBoard.getPits());
+
+        game.makeMove(11, Player.blue);
+        assertArrayEquals(new int[]{1, 9, 8, 1, 2, 9, 2, 1, 1, 9, 9, 0, 9, 11}, testBoard.getPits());
+
+        game.makeMove(4, Player.red);
+        assertArrayEquals(new int[]{1, 9, 8, 1, 0, 10, 3, 1, 1, 9, 9, 0, 9, 11}, testBoard.getPits());
+
+        game.makeMove(3, Player.red);
+        assertArrayEquals(new int[]{1, 9, 8, 0, 0, 10, 5, 1, 0, 9, 9, 0, 9, 11}, testBoard.getPits());
+
+        game.makeMove(7, Player.blue);
+        assertArrayEquals(new int[]{1, 9, 8, 0, 0, 10, 5, 0, 0, 9, 9, 0, 9, 12}, testBoard.getPits());
+
+        game.makeMove(2, Player.red);
+        assertArrayEquals(new int[]{1, 9, 0, 1, 1, 11, 6, 1, 1, 10, 10, 0, 9, 12}, testBoard.getPits());
+
+        game.makeMove(9, Player.blue);
+        assertArrayEquals(new int[]{2, 10, 1, 2, 2, 12, 6, 1, 1, 0, 11, 1, 10, 13}, testBoard.getPits());
+
+        game.makeMove(4, Player.red);
+        assertArrayEquals(new int[]{2, 10, 1, 2, 0, 13, 7, 1, 1, 0, 11, 1, 10, 13}, testBoard.getPits());
+
+        game.makeMove(1, Player.red);
+        assertArrayEquals(new int[]{2, 0, 2, 3, 1, 14, 8, 2, 2, 1, 12, 2, 10, 13}, testBoard.getPits());
+
+        game.makeMove(7, Player.blue);
+        assertArrayEquals(new int[]{2, 0, 2, 3, 1, 14, 8, 0, 3, 2, 12, 2, 10, 13}, testBoard.getPits());
     }
 }
